4526071e1d4c2066fd1dc5d4a4e4ba2bb0677b18
luikdb <luikdb@gmail.com>
2023-07-02 12:27:28 +0530

    asdasd

--- mdps_public.py
+++ mdps_public.py
@@ -40,7 +40,8 @@ with col3:
    
 
 with col1:
-    restecg = st.slider('Resting Electrocardiographic Results: 0 = normal, 1 = having ST-T wave abnormality, 2 = Left Ventricle Hypertrophy', 0,2,1)
+    st.markdown("**Resting Electrocardiographic Results:**")
+    restecg = st.slider('0 = normal, 1 = having ST-T wave abnormality, 2 = Left Ventricle Hypertrophy', 0,2,1)
 
 
 with col2:
